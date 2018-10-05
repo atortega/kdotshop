667988f5e8fc7ca0f352ada12f6c727a46c752e1
elijahpilones <43608529+elijahpilones@users.noreply.github.com>
2018-10-05 17:55:03 +0800

    LOGINGOOGLE

--- app/Http/Controllers/CustomersController.php
+++ app/Http/Controllers/CustomersController.php
@@ -37,7 +37,10 @@ class CustomersController extends Controller
 	public function addCustomer(Request $request)
 	{
         $request->validate([
-            'name'              => 'required|max:30',
+            'first_name'        => 'required|max:30',
+            'last_name'         => 'required|max:30',
+            'birthdate'         => 'required',
+            'gender'            => '',
             'address'           => 'required|max:100',
             'phone_number'      => 'required',
             'email'             => 'required|email|unique:customer,email',
@@ -47,7 +50,11 @@ class CustomersController extends Controller
 
 		$customer = new Customers;
 		
-		$customer->name 		= $request['name'];
+
+        $customer->first_name   = $request['first_name'];
+		$customer->last_name    = $request['last_name'];
+        $customer->birthdate    = $request['birthdate'];
+        $customer->gender       = $request['gender'];
 		$customer->address 		=  $request['address'];
 		$customer->phone_number =  $request['phone_number'];
 		$customer->email 		=  $request['email'];
@@ -105,5 +112,97 @@ class CustomersController extends Controller
         $customer->save();
 
         return array('error' => false, "message"  => "Customer successfully updated!");
+    }
+    public function createCustomer(Request $request)
+    {
+        $request->validate([
+            'email'             => 'required|email|unique:customer,email',
+            'phone_number'      => 'required',
+            'password'          => 'required',
+            'confirm_password'  => 'required|same:password'
+        ]);
+
+        $customer = new Customers;
+        
+        $customer->email        =  $request['email'];
+        $customer->password     =  md5($request['password']);
+        $customer->phone_number =  $request['phone_number'];
+        
+        $customer->save();
+
+        
+        //return $customer;
+
+        //return redirect()->back()->with('message', 'You are successfully registered.');
+
+        return redirect('/login');
+    }
+
+
+    /*
+     * Login Form
+     *
+     */
+    public function loginFormCustomer(Request $request)
+    {
+        //if ($request->session()->get('logged_in')) {
+           // return redirect('/');
+        //}
+        return view('user.templates.page-login');
+    }
+
+    public function loginCustomer(Request $request)
+    {
+        $request->validate([
+            'email'             => 'required|email|exists:customer,email',
+            'password'          => 'required|string|min:8',
+        ]);
+
+
+        $customer = Customers::where("email", $request->email)->first();
+        if (!$customer) {
+            return back()->withErrors('Invalid E-mail!');
+        }
+        
+        if ($customer->password != md5($request->password)) {
+            return back()->withErrors('Password not correct!');
+        }
+
+        //save to session
+        $request->session()->put('logged_in', true);
+        $request->session()->put('email', $request->email);
+        $request->session()->put('customer', $customer);
+
+        //$url = $request->input('url'); 
+
+       //return redirect($url);
+         return redirect('/');
+    }
+    public function forgetPassword(Request $request)
+    {
+        $request->validate([
+            'email'             => 'required|email|unique:customer,email',
+            'new_password'          => 'required|string',
+            'confirm_new_password' => 'required|string',
+        ]);
+
+        $customer = Customers::where("email", $request->email)->first();
+        if (!$customer) {
+            $error="Invalid E-mail!";
+            return false;
+        }
+        
+        if ($customer->password != md5($request->password)) {
+            $error = "Password not correct!";
+            return false;
+        }
+
+        //save to session
+        $request->session()->put('logged_in', true);
+        $request->session()->put('email', $request->email);
+        $request->session()->put('customer', $customer);
+
+        //return redirect()->action('MainController@index');
+        return redirect('/');
     }
 }

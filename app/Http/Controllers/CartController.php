cbaeaeef9f236be5551ad9f9c9a9ce36d2d5873f
gesshchamix <gesshchamix23@gmail.com>
2018-10-17 17:37:46 +0800

    Gleza_21018-10-17_1737
    Fixed Add to Cart

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -22,8 +22,8 @@ class CartController extends Controller
         
         // $cart = Session::get('cart');
         Cart::add([
-            'id'    =>  $product_id,
-            'name'  =>  $productById->product_name,
+            'product_id'    =>  $product_id,
+            'product_name'  =>  $productById->product_name,
             'desc'          =>  $productById->product_desc,
             'price'         =>  $skuById->unit_price,
             'qty'           =>  $request->qty

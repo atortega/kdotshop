ee8adf32e469fd68ac8f1b6d8180dbe184e36668
Aner Ortega <anortega@fullscale.io>
2018-10-30 02:40:41 +0800

    Update database/migrations/2018_10_29_182840_alter_orders_table.php

---
+++ database/migrations/2018_10_29_182840_alter_orders_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AlterOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('orders', function (Blueprint $table) {
+            $table->string('billing_firstname', '50')->nullable()->after('customer_id');
+            $table->string('billing_lastname', '50')->nullable()->after('billing_firstname');
+            $table->string('billing_phone_number', '50')->nullable()->after('billing_lastname');
+            $table->string('shipping_firstname', '50')->nullable()->after('billing_phone_number');
+            $table->string('shipping_lastname', '50')->nullable()->after('shipping_firstname');
+            $table->string('shipping_phone_number', '50')->nullable()->after('shipping_lastname');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}

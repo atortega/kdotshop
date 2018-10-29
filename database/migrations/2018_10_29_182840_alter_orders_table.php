854ca170d0515d7b60e81fb22ad779e5bca9e43c
Aner Ortega <anortega@fullscale.io>
2018-10-30 02:44:43 +0800

    Update database/migrations/2018_10_29_182840_alter_orders_table.php

--- database/migrations/2018_10_29_182840_alter_orders_table.php
+++ database/migrations/2018_10_29_182840_alter_orders_table.php
@@ -17,9 +17,11 @@ class AlterOrdersTable extends Migration
             $table->string('billing_firstname', '50')->nullable()->after('customer_id');
             $table->string('billing_lastname', '50')->nullable()->after('billing_firstname');
             $table->string('billing_phone_number', '50')->nullable()->after('billing_lastname');
-            $table->string('shipping_firstname', '50')->nullable()->after('billing_phone_number');
+            $table->string('billing_email', '50')->nullable()->after('billing_phone_number');
+            $table->string('shipping_firstname', '50')->nullable()->after('billing_email');
             $table->string('shipping_lastname', '50')->nullable()->after('shipping_firstname');
             $table->string('shipping_phone_number', '50')->nullable()->after('shipping_lastname');
+            $table->string('shipping_email', '50')->nullable()->after('shipping_phone_number');
         });
     }
 

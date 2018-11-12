26707540442826e8f01001c8e2faafe9241d5b75
Aner Ortega <anortega@fullscale.io>
2018-11-12 08:49:47 +0800

    bar chart

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use DB;
 
 use App\Models\Customers;
 use App\Models\Orders;
@@ -25,5 +26,35 @@ class AdminController extends Controller
         ];
 
         return view('admin.templates.index', $data);
+    }
+
+    public function getMorrisBarData()
+    {
+        /*
+        $query = Payments::all()
+            ->groupBy(function ($proj) {
+                return $proj->date_paid->format('Ymd');
+            })
+            ->map(function ($day) {
+                return $day->sum('amount');
+            });
+        */
+        $orders = DB::select('SELECT DAY(order_date) as day, SUM(total_amount) as total FROM orders GROUP BY DAY(order_date)');
+
+        foreach($orders AS $row) {
+            if (is_null($row->day)) continue;
+            $data[] = ['y' => $row->day, 'a' => $row->total, 'b' => 0];
+        }
+
+        $paidOrders = DB::select('SELECT DAY(date_paid) as day, SUM(amount) as total FROM payments where reference_code is not null GROUP BY DAY(date_paid)');
+
+        foreach($paidOrders AS $paid) {
+            foreach($data AS &$line) {
+                if ($paid->day == $line['y']) {
+                    $line['b'] = $paid->total;
+                }
+            }
+        }
+        return json_encode($data);
     }
 }
